812663245fd9b18489fb8a4c0ee590157f1d0ec5
fedoweb <fedo.web@mail.ru>
2025-02-06 16:49:45 +0300

    add changes

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,11 +1,6 @@
 export default class Validator {
 
     validateUsername(name) {
-        const regex = /^[a-z]+(?!.*\d{4})[a-z0-9_-]*[a-z]+$/i.test(name);
-
-        if (!regex) {
-            throw new Error("Недопустимое имя пользователя"); 
-        } 
-        return regex;
+        return /^[a-z]+(?!.*\d{4})[a-z0-9_-]*[a-z]+$/i.test(name);
     }
 }
--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -4,22 +4,14 @@ test.each([
   ['test_test', true],
   ['test-test', true],
   ['test123_test', true],
-  ['test-123test', true]
+  ['test-123test', true],
+  ['1test', false],
+  ['test1', false],
+  ['test1234test', false],
+  ['_test', false],
+  ['test-', false]
 ])('testing validateUsername method with valid data', (name, expected) => {
   let validator = new Validator();
 
   expect(validator.validateUsername(name)).toEqual(expected);
-});
-
-test.each([
-  ['1test'],
-  ['test1'],
-  ['test1234test'],
-  ['_test'],
-  ['test-']
-])('testing validateUsername method with invalid data', (name) => {
-  expect(() => {
-    const validator = new Validator();
-    validator.validateUsername(name);
-  }).toThrow(new Error("Недопустимое имя пользователя"));
 });
